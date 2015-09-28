bc95cea8563b9060e5c45ea1e4d5e53cc85b6039
Justyna Piekutowska <piekutowska.justyna@gmail.com>
2015-09-28 22:47:25 +0200

    Improve deeper_patterns

--- deeper_patterns.rb
+++ deeper_patterns.rb
@@ -1,7 +1,7 @@
 def show_regexp(string, pattern)
   match = pattern.match(string)
   if match
-    "#{match.pre_match} --> #{match[0]}<-#{match.post_match}"
+    "#{match.pre_match}->#{match[0]}<-#{match.post_match}"
   else
     "no match"
   end
