de0682ba07d38fb0d3fc990f43c868ea98ddafe4
Justyna Piekutowska <piekutowska.justyna@gmail.com>
2015-09-29 23:10:04 +0200

    Add new options of using regexp

--- character_class98_99.rb
+++ character_class98_99.rb
@@ -18,3 +18,52 @@ puts show_regexp(a, /[A-F ]/)
 puts show_regexp(a, /[A-F][a-f]/)
 puts show_regexp(a, /[0-9]/)
 puts show_regexp(a, /[0-9][0-9]/)
+
+#negation
+puts show_regexp('Price $12.', /[^A-Z]/)
+puts show_regexp('Price $12.', /[^\w]/)
+puts show_regexp('Price $12.', /[a-z][^a-z]/)
+
+puts show_regexp('It costs $12.', /\s/)
+#\d- Decimal digit
+puts show_regexp('It costs $12.', /\d/)
+
+#Unicode or not
+puts show_regexp('już.', /(?a)\w+/)
+puts show_regexp('już.', /(?d)\w+/)
+puts show_regexp('już.', /(?u)\w+/)
+
+puts show_regexp('już.', /(?d)\W+/)
+puts show_regexp('już.', /(?u)\W+/)
+
+#POSIX
+puts show_regexp('Price $12.', /[aeiou]/)
+puts show_regexp('Price $12.', /[[:digit:]]/)
+puts show_regexp('Price $12.', /[[:space:]]/)
+puts show_regexp('Price $12.', /[[:^alpha:]]/)
+puts show_regexp('Price $12.', /[[:punct:]:aeiou]/)
+
+#literal characters in a character class
+a = 'see [the PickAxe-page 123]'
+puts show_regexp(a, /[\]]/)
+puts show_regexp(a, /[0-9\]]/)
+puts show_regexp(a, /[\d\-]/)
+
+#Intersection of character classes
+str = "now is the time"
+str.gsub(/[a-z&&[^aeiou]], '*'/)
+
+#\p
+# encoding" utf-8
+string = "∂y/∂x = 2πx"
+puts show_regexp(string, /\p{Alnum}/)
+puts show_regexp(string, /\p{Digit}/)
+puts show_regexp(string, /\p{Space}/)
+puts show_regexp(string, /\p{Greek}/)
+puts show_regexp(string, /\p{Graph}/)
+
+# period(.)
+a = "It is a time."
+puts show_regexp(a, /i.e/)
+puts show_regexp(a, /./)
+puts show_regexp(a, /\./)
